77cdb07c7629f47fcc23f3c2bf3f8f6ba5d2d05a
Ilyas Garipov <ilyasgaripov97@gmail.com>
2022-05-27 21:38:08 +0300

    Use mouseenter and mouse leave.

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,21 @@ const asideContainer = document.querySelector('aside');
 const skills = document.querySelectorAll('.skill');
 const template = document.createElement('template');
 
-const showSelectedTechnology = (e) => {
-  const title = e.target.querySelector('figcaption.skill__title').innerHTML;
-  const property = title.toLowerCase();
-  asideContainer.insertAdjacentHTML('beforeend', technologies[property]);
-}
-
 const clearAside = () => {
   asideContainer.innerHTML = '';
-}
+};
+
+const showSelectedTechnology = (e) => {
+  const title = e.target.querySelector('figcaption.skill__title');
+  if (title) {
+    clearAside();
+    const property = title.innerHTML.toLowerCase();
+    asideContainer.insertAdjacentHTML('beforeend', technologies[property]);
+  }
+};
 
 skills.forEach((skill) => {
   skill.addEventListener('focus', (e) => showSelectedTechnology(e));
-  skill.addEventListener('hover', (e) => showSelectedTechnology(e))
-  skill.addEventListener('blur', () => clearAside());
+  skill.addEventListener('mouseenter', (e) => showSelectedTechnology(e));
+  skill.addEventListener('mouseleave', () => clearAside());
 });
